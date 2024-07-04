fff15affd6ca881ef350132225ca1469e7865795
Chathuranga Shakthi <chathurangashakthi.k@gmail.com>
2024-07-04 12:50:37 +0530

    add employee form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManageEmployeeComponent } from './page/manage-employee/manage-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ManageEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/page/manage-employee/manage-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-employee.component.html',
+  styleUrl: './manage-employee.component.css'
+})
+export class ManageEmployeeComponent {
+
+}
